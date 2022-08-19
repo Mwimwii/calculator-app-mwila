1335af4ab508288f8153f49fd17f007ed7382a32
gooby <mpnyirongo@gmail.com>
2022-08-19 10:19:00 +0200

    Added 404 page

---
+++ src/pages/page-not-found/index.tsx
@@ -0,0 +1,15 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+
+const PageNotFound: React.FC = () => {
+  return (
+    <>
+      <Typography variant="h1" color="black">
+        Page not found
+      </Typography>
+    </>
+  )
+}
+
+export default PageNotFound
