8632fb9197a25a632dbedbfcdb81be65be3d8ccf
gooby <mpnyirongo@gmail.com>
2022-08-19 10:38:53 +0200

    Added calculator view content

--- src/components/calculator-view/index.tsx
+++ src/components/calculator-view/index.tsx
@@ -1,8 +1,28 @@
+import { Container, Typography } from '@mui/material'
 import React from 'react'
+import { CalculatorExpression } from '../../types'
 
-const CalculatorView: React.FC = () => {
+
+const CalculatorView: React.FC<{expression: CalculatorExpression}> = () => {
   return (
     <>
+    {/* create a container for the calculator view with a black background */}
+    <Container style={{
+      backgroundColor: '#121212',
+      borderRadius: 10,
+      padding: '0px',
+      margin: '0px'
+    }}>
+      {/* we need some typography with purple font */}
+      <Typography variant="h2" style={{color:"#A160FB"}}>
+        {/* the expression will be displayed here */}
+        234+74
+      </Typography>
+      <Typography variant="h1" color="white">
+        {/* the expression will be displayed here */}
+        308
+      </Typography>
+    </Container>
     </>
   )
 }
