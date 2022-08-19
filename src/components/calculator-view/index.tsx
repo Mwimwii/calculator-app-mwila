92b026e358144e60f1eacdc967131975358f611d
gooby <mpnyirongo@gmail.com>
2022-08-19 12:06:24 +0200

    Added the icons from the design spec

--- src/components/calculator-view/index.tsx
+++ src/components/calculator-view/index.tsx
@@ -1,7 +1,8 @@
-import { Container, Typography } from '@mui/material'
+import { Container, Typography, Grid } from '@mui/material'
 import React from 'react'
 import { CalculatorExpression } from '../../types'
-
+import MenuIcon from '@mui/icons-material/Menu';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const CalculatorView: React.FC<{expression: CalculatorExpression}> = () => {
   return (
@@ -15,16 +16,29 @@ const CalculatorView: React.FC<{expression: CalculatorExpression}> = () => {
       borderBottomRightRadius: 10,
       padding: 0,
       width: "300px",
+      height: 180
     }}>
+      <Grid container columns={{xs: 12}} alignItems="center" justifyContent="space-between" style={{paddingTop: 20}}>
+        <Grid item xs={2}>
+          <MenuIcon style={{color: "white", verticalAlign:"middle"}}/>
+        </Grid>
+   
+        <Grid item xs={2}>
+          <AccountCircleIcon style={{color: "white", verticalAlign:"middle"}} />
+        </Grid>
+      </Grid>
+        
+
       {/* we need some typography with purple font */}
-      <Typography variant="h2" style={{color:"#A160FB"}}>
+      <Typography variant="h5" style={{color:"#A160FB"}}>
         {/* the expression will be displayed here */}
         234+74
       </Typography>
-      <Typography variant="h1" color="white">
+      <Typography variant="h3" color="white">
         {/* the expression will be displayed here */}
         308
       </Typography>
+
     </Container>
     </>
   )
