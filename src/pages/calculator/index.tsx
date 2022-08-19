50f86dabf42f7e7aaa752e5e7aa79cad6d29356a
gooby <mpnyirongo@gmail.com>
2022-08-19 11:38:33 +0200

    Modified buttons to support hover

--- src/pages/calculator/index.tsx
+++ src/pages/calculator/index.tsx
@@ -1,25 +1,36 @@
 import React from 'react'
 import { Button, Container, Grid } from '@mui/material'
-import CalculatorView from '../../components/calculator-view'
+import { CalculatorView } from '../../components'
 
 const useInputs = () => {
+  const size = 60
+  const style ={height: size,  width: size, margin: "0", fontSize:"20px", minWidth: 0, padding: 5,
+  boxShadow:  "9px 9px 18px #e2e2e2, -9px -9px 18px #fefefe", borderRadius: 20,
+  // hover
+  "&:hover": {
+    boxShadow:  "9px 9px 18px #e2e2e2, -9px -9px 18px #fefefe",
+    backgroundColor: "black",
+    height: 200
+  }
+} 
+
   const createNumbers = () => {
     const calculatorMap = ["%", "CE", "AC", "/", 7, 8, 9, "x", 4, 5, 6, "-", 1, 2, 3, "+", " ", 0, ".", "="]
     const numbers = calculatorMap.map((sym: string | number, i) => {
       if(sym === "=") {
         return( 
         <Grid item xs={1} key={i}>
-          <Button variant="contained" style={{height: 55, margin: "0", backgroundColor: "#A160FB", color: "white", fontSize:"30px"}}>{sym}</Button>
+          <Button variant="contained" style={{...style, backgroundColor: "#A160FB", color: "white"}}>{sym}</Button>
         </Grid>)
         }
         else if(isNaN(sym as number)) {
           return(<Grid item xs={1} key={i}>
-            <Button variant="contained" style={{height: 55, margin: "0", backgroundColor: "#F0F0F0", color: "#7F7F7F", fontSize:"30px"}}>{sym}</Button>
+            <Button variant="contained" style={{...style, backgroundColor: "#F0F0F0", color: "#7F7F7F"}}>{sym}</Button>
           </Grid>)}
         else 
         {
           return(<Grid item xs={1} key={i}>
-            <Button variant="contained" style={{height: 55, margin: "0", backgroundColor: "#F0F0F0", color: "#515151", fontSize:"30px"}}>{sym}</Button>
+            <Button variant="contained" style={{...style, backgroundColor: "#F0F0F0", color: "#515151"}}>{sym}</Button>
           </Grid>)}
   })
     return numbers
@@ -42,16 +53,23 @@ const CalculatorGrid: React.FC = () => {
   const { createNumbers } = useInputs()
   return (
     <>
-    <Container maxWidth="xs" style={{backgroundColor: '#F0F0F0', borderRadius: 10}}> 
-    <CalculatorView expression={{
-      answer: "",
-      firstInput: "",
-      secondInput: "",
-      operator: "",
-    }}/>
-    <Grid container spacing={2} justifyItems='center' columns={{xs: 4}} style={{padding: "40px"}}>
-     {createNumbers()}
-    </Grid>
+    <Container style={{
+      maxWidth: '300px',
+      backgroundColor: '#F0F0F0',
+      borderRadius: 30, 
+      padding: 0,
+    }}
+    > 
+        <CalculatorView expression={{
+          answer: "",
+          firstInput: "",
+          secondInput: "",
+          operator: "",
+        }}/>
+
+        <Grid container spacing={2} columns={{xs: 4}} style={{padding: "20px", paddingBottom: 30, backgroundColor:"#F1F1F1", maxWidth:"300px", minWidth: 0}}>
+          {createNumbers()}
+        </Grid>
     </Container>
     </>
   )
