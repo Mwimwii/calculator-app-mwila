70d7f1c85636562ca855dfe49e945438b455343b
gooby <mpnyirongo@gmail.com>
2022-08-22 12:15:11 +0200

    Moved display state to custom hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,6 +2,7 @@ import React from 'react'
 import { CalculatorExpression } from '../types'
 
 export const useCalculatorStack = () => {
+  const [display, setDisplay] = React.useState("");
   const [expression , setExpression] = React.useState<CalculatorExpression>({
     answer: "",
     firstInput: "",
@@ -9,13 +10,102 @@ export const useCalculatorStack = () => {
     operator: ""
   } as CalculatorExpression);
   const [stack, setStack] = React.useState<CalculatorExpression[]>([]);
+
+  const evaluate = (expression: CalculatorExpression) => {
+    const { firstInput, secondInput, operator } = expression;
+    let answer = "";
+    console.log('Operator', operator);
+    switch (operator) {
+      case "+":
+        answer = (parseFloat(firstInput) + parseFloat(secondInput)).toString();
+        break;
+      case "-":
+        answer = (parseFloat(firstInput) - parseFloat(secondInput)).toString();
+        break;
+      case "*":
+        answer = (parseFloat(firstInput) * parseFloat(secondInput)).toString();
+        break;
+      case "/":
+        answer = (parseFloat(firstInput) / parseFloat(secondInput)).toString();
+        if (secondInput === '0') {
+          answer = "0";
+          return('Cannot Divide by zero')
+        }
+        break;
+      default:
+        console.log('Nothing')
+    }
+    return answer;
+  }
  
-  const setInput = (input: string) => {
-    setExpression({...expression, answer: input})
+  React.useEffect(() => {
+    console.log(expression)
+  }, [expression])
+
+  const setInput = (displayInput: string, input: string) => {
+    if(input === "AC"){
+      setExpression({ ...expression, firstInput: "", secondInput: "", operator: "" });
+      setDisplay("");
+      return
+    } else if(input === "CE"){
+      (expression.firstInput !== "" && expression.operator === "") && setExpression({ ...expression, firstInput:"" });
+      expression.secondInput !== "" && setExpression({ ...expression, secondInput:"" });
+      setDisplay("");
+      return
+    } else if (input === "-/+") {
+      if (expression.secondInput !== "") {
+       setExpression({ ...expression, secondInput: expression.secondInput.charAt(0) === "-" ? expression.secondInput.slice(1) : "-" + expression.secondInput });
+       setDisplay(expression.secondInput);
+     }
+     else
+      if (expression.firstInput !== "") {
+        setExpression({ ...expression, firstInput: expression.firstInput.charAt(0) === "-" ? expression.firstInput.slice(1) : "-" + expression.firstInput });
+        setDisplay(expression.firstInput);
+      } 
+      return
+    } else if (input === "+" || input === "-" || input === "*" || input === "/") {
+      if(expression.firstInput === "") {
+       return 
+      }
+      else if (expression.operator === "" || expression.secondInput === "")
+        setExpression({ ...expression, operator: input });
+      else if (expression.secondInput === "" && expression.operator !== "")
+        setExpression({ ...expression, operator: input });
+      else if (expression.secondInput === "") {
+        setStack([...stack, expression]);
+        setExpression({ ...expression, firstInput:expression.answer , secondInput: "", operator: input, answer: "" });
+      } else {
+        setStack([...stack, expression]);
+        setExpression({ ...expression, firstInput: evaluate(expression), secondInput: "", operator: input, answer: "" });
+      }
+    } else if (input === "%") {
+      expression.answer !== "" && setExpression({ ...expression, answer: (Number (expression.secondInput)/100).toString() });
+      (expression.firstInput !== "" && expression.operator === "") && setExpression({ ...expression, firstInput:(Number (expression.firstInput)/100).toString() });
+      expression.secondInput !== "" && setExpression({ ...expression, secondInput: (Number (expression.secondInput)/100).toString() });
+
+    } 
+    else if (input === "=") {
+      if (expression.answer !== "") {
+        setExpression({ ...expression, firstInput: expression.answer });
+      }
+      setExpression({ ...expression, answer: evaluate(expression) });
+      setStack([...stack, expression]);
+      setDisplay(evaluate(expression));
+      return
+    } else {
+      if (!expression.operator) {
+        setExpression({ ...expression, firstInput: expression.firstInput + input });
+      } else {
+        setExpression({ ...expression, secondInput: expression.secondInput + input });
+      } 
+      return
+    }
   }
   return {
     expression,
-    setInput
+    setInput,
+    display,
+    setDisplay
   } as const
 }
   
--- src/pages/calculator/index.tsx
+++ src/pages/calculator/index.tsx
@@ -1,63 +1,59 @@
 import React from 'react'
 import { Button, Container, Grid } from '@mui/material'
 import { CalculatorView } from '../../components'
+import { useCalculatorStack } from '../../hooks'
 
-const useInputs = () => {
-  const [display, setDisplay] = React.useState("");
+const useInputs = (setInput:(displayInput:string, input: string) => void) => {
   const size = 60
   const style ={height: size,  width: size, margin: "0", fontSize:"20px", minWidth: 0, padding: 5,
-  boxShadow:  "9px 9px 18px #e2e2e2, -9px -9px 18px #fefefe", borderRadius: 20,
-  // hover
-  "&:hover": {
-    boxShadow:  "9px 9px 18px #e2e2e2, -9px -9px 18px #fefefe",
-    backgroundColor: "black",
-    height: 200
-  }
+  boxShadow:  "9px 9px 18px #e2e2e2, -9px -9px 18px #fefefe", borderRadius: 20
 } 
 
+const handleInput = (displayInput: string, input: string) => {
+  setInput(displayInput, input)
+} 
   const createNumbers = () => {
-    const calculatorMap = ["%", "CE", "AC", "/", 7, 8, 9, "x", 4, 5, 6, "-", 1, 2, 3, "+", " ", 0, ".", "="]
+    const calculatorMap = ["%", "CE", "AC", "/", 7, 8, 9, "*", 4, 5, 6, "-", 1, 2, 3, "+", "-/+", 0, ".", "="]
     const numbers = calculatorMap.map((sym: string | number, i) => {
       if(sym === "=") {
         return( 
         <Grid item xs={1} key={i}>
           <Button variant="contained" style={{...style, backgroundColor: "#A160FB", color: "white"}}
-            onClick={() => setDisplay("")}>{sym}</Button>
+            onClick={() => {handleInput("=", sym)}}>{sym}</Button>
         </Grid>)
         } 
       if(sym === ".") {
         return( 
         <Grid item xs={1} key={i}>
           <Button variant="contained" style={{...style, backgroundColor: "#F0F0F0", color: "#7F7F7F"}}
-            onClick={() => setDisplay(display + sym)}>{sym}</Button>
+            onClick={() => {handleInput(sym, sym)}}>{sym}</Button>
         </Grid>)
         } 
-        else if(isNaN(sym as number)) {
+        else if(isNaN(Number(sym))) {
           return(<Grid item xs={1} key={i}>
             <Button variant="contained" style={{...style, backgroundColor: "#F0F0F0", color: "#7F7F7F"}}
-            onClick={() => setDisplay("")}>{sym}</Button>
+            onClick={() => {handleInput("", sym.toString())} }>{sym}</Button>
           </Grid>)}
         else 
         {
           return(<Grid item xs={1} key={i}>
             <Button variant="contained" style={{...style, backgroundColor: "#F0F0F0", color: "#515151"}}
-            onClick={() => setDisplay(display + sym)}>{sym}</Button>
+            onClick={() => {handleInput(sym.toString(), sym.toString())} }>{sym}</Button>
           </Grid>)}
   })
     return numbers
   }
+
   return {
-    display,
     createNumbers
   } as const
 }
 
 
 const CalculatorGrid: React.FC = () => {
-  const { display, createNumbers } = useInputs()
-  React.useEffect(() => {
-    console.log(display)
-  },[display])
+  const {expression, setInput, display} = useCalculatorStack()
+  const { createNumbers } = useInputs(setInput)
+
   return (
     <>
     <Container style={{
@@ -67,12 +63,8 @@ const CalculatorGrid: React.FC = () => {
       padding: 0,
     }}
     > 
-        <CalculatorView expression={{
-          answer: "",
-          firstInput: "",
-          secondInput: "",
-          operator: "",
-        }}
+        <CalculatorView expression={expression} 
+        display={display}
         />
 
         <Grid container spacing={2} columns={{xs: 4}} style={{padding: "20px", paddingBottom: 30, maxWidth:"300px", minWidth: 0}}>
