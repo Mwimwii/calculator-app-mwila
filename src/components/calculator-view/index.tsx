feefd7b942bc6b509612ada3c8cbc6fa707d9f1a
gooby <mpnyirongo@gmail.com>
2022-08-22 12:15:29 +0200

    Formatting

--- src/components/calculator-view/index.tsx
+++ src/components/calculator-view/index.tsx
@@ -3,41 +3,46 @@ import React from 'react'
 import { CalculatorExpression } from '../../types'
 import MenuIcon from '@mui/icons-material/Menu';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { Link } from 'react-router-dom';
 
-const CalculatorView: React.FC<{expression: CalculatorExpression}> = () => {
+interface CalculatorViewProps {
+  expression: CalculatorExpression
+  display: string
+}
+
+const CalculatorView: React.FC<CalculatorViewProps> = ({expression, display}) => {
   return (
     <>
-    {/* create a container for the calculator view with a black background */}
     <Container style={{
       backgroundColor: '#121212',
       borderTopLeftRadius: 30,
       borderTopRightRadius: 30,
       borderBottomLeftRadius: 10,
-      borderBottomRightRadius: 10,
+      borderBottomRightRadius: 10, 
       padding: 0,
       width: "300px",
-      height: 180
+      height: 200
     }}>
-      <Grid container columns={{xs: 12}} alignItems="center" justifyContent="space-between" style={{paddingTop: 20}}>
+      <Grid container spacing={0} columns={{xs: 12}} alignItems="center" justifyContent="space-between" style={{paddingTop: 20}}>
         <Grid item xs={2}>
-          <MenuIcon style={{color: "white", verticalAlign:"middle"}}/>
+          <Link to={'/'}><MenuIcon style={{color: "white", verticalAlign:"middle"}}/></Link>
         </Grid>
    
         <Grid item xs={2}>
-          <AccountCircleIcon style={{color: "white", verticalAlign:"middle"}} />
+          <Link to={'/'}><AccountCircleIcon style={{color: "white", verticalAlign:"middle"}} /></Link>
+        </Grid>
+
+        <Grid item xs={12} style={{paddingTop: 60, paddingRight: 30}}>
+          <Typography variant="h6" style={{color:"#A160FB"}} textAlign="end">
+            {expression && `${expression.firstInput === "" ? "" : expression.firstInput} ${expression.operator === "" ? "" : expression.operator} ${expression.secondInput === "" ? "" : expression.secondInput}`}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} style={{paddingTop: 0,paddingRight: 30}}>
+          <Typography variant="h4" color="white" textAlign="end">
+            {display === "" ? "" : display}
+          </Typography>
         </Grid>
       </Grid>
-        
-
-      {/* we need some typography with purple font */}
-      <Typography variant="h5" style={{color:"#A160FB"}}>
-        {/* the expression will be displayed here */}
-        234+74
-      </Typography>
-      <Typography variant="h3" color="white">
-        {/* the expression will be displayed here */}
-        308
-      </Typography>
 
     </Container>
     </>
